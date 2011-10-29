72375059e47c30c3fe75ce6a26373cc50db34c03
Gaurav Arora <gaurav.arora6@gmail.com>
2011-10-29 21:32:48 +0530

    Adding first file

---
+++ welcom2GitHub.cs
@@ -0,0 +1,16 @@
+// File name : welcome2GitHub.cs
+
+using System;
+namespace CSharp.AStepAhead.welcome2CS
+{
+	class welcome2GitHub
+	{
+
+		static void Main()
+		{
+			Console.WriteLine("Welcome to C# - This is my First Step");
+			Console.ReadLine();
+			return;
+		}
+	}
+}
